46f07fed888470af4eee399f43036c896b2c9627
marwanrawshedh <marwansalem55@yahoo.com>
2021-11-08 22:34:42 +0200

    struggle

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models/index.js')
+
+module.exports = async (req, res, next) => {
+  // const basic = req.headers.authorization.split(' ')[1];
+  if (!req.headers.authorization) { return _authError(); }
+  const basic = req.headers.authorization.split(' ')[1];
+  
+  // let basic = req.headers.authorization;
+  let [username, pass] = base64.decode(basic).split(':');
+  // console.log(  await users.authenticateBasic(username, pass))
+  try {
+    
+    req.user = await users.authenticateBasic(username, pass)
+    // console.log(req.user)
+    next();
+  } catch (e) {
+    res.status(403).send('Invalid Login');
+  }
+
+}
